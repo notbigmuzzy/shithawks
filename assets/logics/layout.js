4b018a5fbde9c810e66e2c41ce6ed5d317733ff7
NBM <notbigmuzzy@gmail.com>
2023-01-19 18:02:12 +0100

    Layout setup

---
+++ assets/logics/layout.js
@@ -0,0 +1,34 @@
+export const layout = () => {
+	window.onhashchange = urlEventListeners;
+	function urlEventListeners() {
+		let e = location.hash,
+			currentTab = document.querySelector('[href="' + e + '"]'),
+			currentPage = document.getElementById(e.substring(1));
+
+		let getSiblings = function (elem) {
+			let siblings = [];
+			let sibling = elem.parentNode.firstChild;
+			while (sibling) {
+				if (sibling.nodeType === 1 && sibling !== elem) {
+					siblings.push(sibling);
+				}
+				sibling = sibling.nextSibling
+			}
+			return siblings;
+		};
+		let otherTabs = getSiblings(currentTab);
+		otherTabs.forEach(function(otherTab) {
+			otherTab.classList.remove('active-tab');
+		})
+		currentTab.classList.add('active-tab');
+		let otherPages = getSiblings(currentPage);
+		otherPages.forEach(function(otherPage) {
+			otherPage.classList.remove('active-page');
+		})
+		currentPage.classList.add('active-page');
+	}
+	urlEventListeners();
+
+
+
+}
---
+++ index.js
@@ -0,0 +1,3 @@
+import { layout } from './assets/logics/layout.js';
+
+layout();
