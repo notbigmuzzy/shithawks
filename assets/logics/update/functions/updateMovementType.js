f299def47f6873e4d5eb48f58f8ef7059c526c4b
NBM <notbigmuzzy@gmail.com>
2023-02-02 13:42:10 +0100

    Update map

--- assets/logics/update/functions/updateMovementType.js
+++ assets/logics/update/functions/updateMovementType.js
@@ -16,3 +16,22 @@ export const updateMovementType = (pos, field, msg) => {
 	//UPDATE ALL STATS FIELD
 	field.html('<p>' + msg + '</p>')
 }
+
+export const updateMovementLog = (pos, field, msg) => {
+	if (pos.hasClass('has-car')) {
+		msg = "You're in a car!"
+	} else if (pos.hasClass('has-boat')) {
+		msg = "You're on a boat!"
+	} else if (pos.hasClass('city')) {
+		if (pos.hasClass('hideout')) {
+			msg = "You're in your hideout!"
+		} else {
+			msg = "You're in a city!"
+		}
+	} else {
+		msg = "You're on foot!"
+	}
+	
+	//UPDATE ALL STATS FIELD
+	field.prepend('<p>' + msg + '</p>')
+}
--- assets/logics/update/updateMapPage.js
+++ assets/logics/update/updateMapPage.js
@@ -5,8 +5,14 @@ import { updateDate } from "./functions/stats/updateDate.js"
 import { resetEnergy } from "./functions/stats/resetEnergy.js"
 import { updateHealth } from "./functions/stats/updateHealth.js"
 import { updateRefill } from "./functions/stats/updateRefill.js"
+import { updateMovementLog } from "./functions/updateMovementType.js"
 
 export const updateMapPage = () => {
+	var currentCharacterPosition = $('.has-character'),
+		currentCharacterPositionSprite = currentCharacterPosition.find('.sprite'),
+		updateMovementStatModal = $('.junfo-log modal-body'),
+		updateMovementStatMessage = "No info";
+
 	playerPosition();
 	carPosition();
 	boatPosition();
@@ -14,4 +20,7 @@ export const updateMapPage = () => {
 	resetEnergy();
 	updateHealth();
 	updateRefill();
+
+	//INIT UPDATE FUNCTIONS
+	updateMovementLog(currentCharacterPosition,updateMovementStatModal,updateMovementStatMessage);
 }
