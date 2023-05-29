6b2bc3fb6c6dc1fcf33e0c5a2ee96f9280a81668
NBM <notbigmuzzy@gmail.com>
2023-05-29 13:58:35 +0200

    Fix minior glitch with summoning transport

--- assets/logics/movement/summonBoat.js
+++ assets/logics/movement/summonBoat.js
@@ -2,5 +2,7 @@ export const summonBoat = (fieldMovingTo) => {
 	$('.has-boat').removeClass('has-boat').find('in-boat').remove();
 	fieldMovingTo.addClass('has-boat').removeClass('summon-boat').append(window.saveState.characters.boat);
 	let currentBoatPosition = $('.has-boat').attr("data-ordinal");
+	$('.summon-car').removeClass('summon-car, can-have-player');
+	$('.summon-boat').removeClass('summon-boat, can-have-player');
 	window.saveState.stats.boatordinal = currentBoatPosition;
 }
--- assets/logics/movement/summonCar.js
+++ assets/logics/movement/summonCar.js
@@ -2,5 +2,7 @@ export const summonCar = (fieldMovingTo) => {
 	$('.has-car').removeClass('has-car').find('in-car').remove();
 	fieldMovingTo.addClass('has-car').removeClass('summon-car').append(window.saveState.characters.car);
 	let currentCarPosition = $('.has-car').attr("data-ordinal");
+	$('.summon-car').removeClass('summon-car, can-have-player');
+	$('.summon-boat').removeClass('summon-boat, can-have-player');
 	window.saveState.stats.carordinal = currentCarPosition;
 }
